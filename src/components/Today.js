656c4a7b85615ca85093c62bf1bfa0d6d4679d0f
Satyakiran Patnala <75940037+galaxey56@users.noreply.github.com>
2022-09-28 11:14:05 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -5,16 +5,7 @@ const Today = (props) => {
 ;
 
   const filteredList = props.list.filter((task) => {
-    if (date.getFullYear() !== task.date.getFullYear()) {
-      return false;
-    }
-    if (date.getMonth() !== task.date.getMonth()) {
-      return false;
-    }
-    if (date.getDate() !== task.date.getDate()) {
-      return false;
-    }
-    return true;
+   
   });
 
 
