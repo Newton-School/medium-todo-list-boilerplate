291da2d227184d853104c5bb1b0b55f1c4d9c816
Satyakiran Patnala <75940037+galaxey56@users.noreply.github.com>
2022-09-28 11:14:48 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -4,12 +4,7 @@ const Next7Days = (props) => {
   const date = new Date();
 
   const filteredList = props.list.filter((task) => {
-    const diffTime = Math.abs(task.date - date);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    if (diffDays < 8) {
-      return true;
-    }
-    return false;
+  
   });
 
 
