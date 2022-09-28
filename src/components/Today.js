71e14a994d7d11b35b35516f126718ee48e934c8
galaxey56 <satyakiran56@gmail.com>
2022-09-28 14:20:11 +0530

    changed

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,34 +1,12 @@
 import React from "react";
+import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-;
-
-  const filteredList = props.list.filter((task) => {
-    if (date.getFullYear() !== task.date.getFullYear()) {
-      return false;
-    }
-    if (date.getMonth() !== task.date.getMonth()) {
-      return false;
-    }
-    if (date.getDate() !== task.date.getDate()) {
-      return false;
-    }
-    return true;
-  });
-
-
+  
   return (
     <div id="today-list">
-      {filteredList.map((list) => {
-        return (
-
-            <div className="box" key={} >
-              <div className="task">{} {" "} ({})</div>
-            </div>
-          
-        );
-      })}
+      <ListRender list={} />
     </div>
   );
 };
